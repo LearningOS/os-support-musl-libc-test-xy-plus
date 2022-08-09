6e8cf5c5467c2db62f716316fc32659cdae640b3
小源 <511568816@qq.com>
2022-08-09 17:43:57 +0800

    update sh

--- runos.sh
+++ runos.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
-cd os 
-timeout 1800 make run > ../output
+
+cd os-ref/os
+make build
+cp ./target/riscv64gc-unknown-none-elf/release/os.bin ../../os/
+cd ../../os
+./compress.sh
+make run
+# timeout 1800 make run > ../output
