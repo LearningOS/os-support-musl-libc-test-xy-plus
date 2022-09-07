2885245651ace4b67ba81093e274e177156f6d9d
luffy <511568816@qq.com>
2022-09-07 11:31:30 +0800

    t error to t printf

--- testsuits-for-os/libc-test/src/common/test.h
+++ testsuits-for-os/libc-test/src/common/test.h
@@ -6,7 +6,8 @@ extern volatile int t_status;
 
 #define T_LOC2(l) __FILE__ ":" #l
 #define T_LOC1(l) T_LOC2(l)
-#define t_error(...) t_printf(T_LOC1(__LINE__) ": " __VA_ARGS__)
+// #define t_error(...) t_printf(T_LOC1(__LINE__) ": " __VA_ARGS__)
+#define t_error t_printf
 
 int t_printf(const char *s, ...);
 
